9be51e7001f88beb8634ac3acae6660337e673c7
Brian Jackson <bjack205@gmail.com>
2018-11-13 07:21:51 -0800

    Finish up

--- src/project_functions.jl
+++ src/project_functions.jl
@@ -1,3 +1,16 @@
 newfun(x::Int) = x + 3
 newfun(x::Float64) = x + π
 newfun(x::String) = x * " π"
+
+function myrandsolve()
+    N = 500
+    A = rand(N,N)
+    b = rand(N)
+    x = A\b
+
+    c = zero(b)
+    for i = 1:N
+        c[i] = newfun(x[i])
+    end
+    c
+end
